95150683289a793bd398178383f2d86718c3e352
Igor Fernandes <igorrfc10@gmail.com>
2017-07-12 23:31:52 -0300

    Update the ApplicationController
    adding the current_user helper method which holds the user stored on the session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
+# ApplicationController - The application main controller
 class ApplicationController < ActionController::API
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
