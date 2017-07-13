5321026b2ff19d8e9c4b243ac1d407e1c2cbbd87
Igor Fernandes <igorrfc10@gmail.com>
2017-07-13 00:14:17 -0300

    Create the SessionsController
    Improvements:
    
      - Enabled the create and destroy actions on the controller;
      - The route to those actions was configured;
      - Specs created for the post #create and delete #destroy;
      - Swagger blocks specification for the actions;
    
    fixes #9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :docs, only: [:index]
 
+  resources :sessions, only: [:create]
+  delete '/logout', to: 'sessions#destroy'
+
   namespace :api do
     scope module: :v1 do
       resources :users, only: [:create]
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe SessionsController, type: :controller do
+  describe 'POST #create' do
+    let(:email) { 'foo@bar.com' }
+
+    context 'when there is an user matching the email received and with the correct password' do
+      let(:password) { 'foopassword' }
+      let!(:user) { create(:user, email: email, password: password, password_confirmation: password) }
+
+      before { post :create, params: { email: email, password: password } }
+
+      it 'returns a "success" http response' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'updates the session[:user_id] with the received user' do
+        expect(session[:user_id]).to eq user.id
+      end
+    end
+
+    context 'when there is no user with the email received as parameter' do
+      before { post :create, params: { email: email } }
+
+      it 'returns a "unprocessable_entity" http response' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns an invalid email/password error' do
+        expect(hash_format_response[:errors])
+          .to include(authentication: ['You have entered incorrect email and/or password'])
+      end
+    end
+  end
+
+  describe 'POST #destroy' do
+    before { delete :destroy }
+
+    it 'returns a "success" http response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'removes the session[:user_id]' do
+      expect(session[:user_id]).to be_nil
+    end
+  end
+end
