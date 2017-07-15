4571207cd47c6e751f06f5382bb987bae5396514
Igor Fernandes <igorrfc10@gmail.com>
2017-07-15 19:18:53 -0300

    Update the config/application.rb file
    removing the api_only configuration.

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,5 @@ module MicroblogApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.version = '1.0.0'
-    config.api_only = true
   end
 end
