1860998d5b0fb8a0cf961e8b6ab546fe0cd61eaa
Igor Fernandes <igorrfc10@gmail.com>
2017-07-16 00:27:31 -0300

    Update the UserSerializer
    adding the 'notifications' associated resource on the serialization.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 # UserSerializer - prepares the JSON format of the user resource for API's responses
 class UserSerializer
   def self.serialize(resource)
-    resource.as_json(except: :password_digest, include: %i[posts followers followees])
+    resource.as_json(except: :password_digest, include: %i[posts followers followees notifications])
   end
 end
